26d7d6e891e14ca32403d0b8e8d7254ccb42a163
surajllpl <srj.llpl@gmail.com>
2023-12-05 18:35:49 +0530

    calculator history

--- cal_menu.py
+++ cal_menu.py
@@ -1,72 +1,114 @@
 
 print("!!.....WELCOME.....!!")
 
-f_no = float(input("Enter first no :- "))
+f_no = int(input("Enter first no :- "))
 op = input("Enter operation: \n 1. Addition \n 2. Substraction \n 3. Multipication \n 4. Divided \n ")
-s_no = float(input("Enter second no :- "))
+s_no = int(input("Enter second no :- "))
 
-res =[0]
-i=0
-
+history = [] # empty list declaration
+res = []
 
 
 def add(a,b):
     
     result=a+b
-    res[i]=result
+    ans = f"{a} + {b} =",result
+    history.append(ans)
+    res.append(result)
     print("Addition = ",result)
-    # con=1  
+    return result
 
+   
 def sub(a,b):
     result=a-b
-    res[i]=result
+    ans = f"{a} - {b} =",result
+    history.append(ans)
+    res.append(result)
     print ("substraction = ",result)
+    return result
+
 
 def mul(a,b):
     result=a*b
-    res[i]=result
+    ans = f"{a} * {b} =",result
+    history.append(ans)
+    res.append(result)
     print ("Multiplication",result)
+    return result
+
 
 def div(a,b):
     result=a/b
-    res[i]=result
+    ans = f"{a} / {b} =",result
+    history.append(ans)
+    res.append(result)
     print ("Divided = ",result)
+    return result
+
 temp=0
 con=0
-# result=0
+i=0
+
 while(temp==0):
    
    if(con==1):
     
-    print("result =  ", res[i])
-    op = input("Enter operation: \n 1. Addition \n 2. Substraction \n 3. Multipication \n 4. Divided \n 5. Exit\n")
+    print("ans  =  ", res)
+    op = input("Enter operation: \n 1. Addition \n 2. Substraction \n 3. Multipication \n 4. Divided \n 5. clear\n 6. History\n 7. Exit\n")
+       
     if(op=="5"):
-        break
-    else:
-        s_no = float(input("Enter second no :- "))
-        f_no = res[i]
-        con=0
+        history.clear()
+        ans=" "
+        print("History Clear...\n")
+        con=1 
+    elif(op=="6"):
+            print(history)
+            ans=" "
+    elif(op=="7"):
+            break
+    else:       
+                f_no = res[i]
+                i+=1
+                con=0
+                s_no = int(input("Enter second no :- "))
+            
+                
+    
+       
    else:
-    match op:
+       match op:
         case "1":
+          
             add(f_no,s_no)
+            
             con=1
         case "2":
+
             sub(f_no,s_no)
             con=1
         case "3":
+
             mul(f_no,s_no)
             con=1
         case "4":
+
             div(f_no,s_no)
             con=1
-        case "5":
-            # break
-            temp=1
+        # case "5":
+           
+             
+        # case "6":
+             
+        #     print(history)
+       
+             
+        # case "7":
+        #     break
            
           
 else:
-     print("exit")
+    print("exit")
+
 
 
 
